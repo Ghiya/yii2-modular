b05a3397c39bffa5a6e5ff9fa12f0d1bcc360117
Ghiya <ghiya.mikadze@gmail.com>
2018-07-25 17:56:51 +0300

    core controller updates

--- src/core/Controller.php
+++ src/core/Controller.php
@@ -7,6 +7,7 @@
 namespace modular\core;
 
 use modular\core\helpers\ArrayHelper;
+use modular\panel\models\User;
 use modular\panel\PanelModule;
 use modular\resource\ResourceModule;
 
@@ -30,10 +31,54 @@ abstract class Controller extends \yii\web\Controller
 
     /**
      * {@inheritdoc}
+     *
+     * @throws \Throwable
      */
     public function render($view, $params = [])
     {
-        return parent::render($view, ArrayHelper::merge(['breadcrumb' => $this->breadcrumb], $params));
+        return
+            parent::render(
+                $this->module instanceof PanelModule ?
+                    $this->viewWithRole($view) : $view,
+                ArrayHelper::merge(
+                    [
+                        'breadcrumb' => $this->breadcrumb
+                    ],
+                    $params
+                )
+            );
+    }
+
+
+    /**
+     * {@inheritdoc}
+     *
+     * @throws \Throwable
+     */
+    public function renderPartial($view, $params = [])
+    {
+        return
+            parent::renderPartial(
+                $this->module instanceof PanelModule ?
+                    $this->viewWithRole($view) : $view,
+                $params);
+    }
+
+
+    /**
+     * @param string $view
+     *
+     * @return string
+     * @throws \Throwable
+     */
+    protected function viewWithRole($view = "")
+    {
+        /** @var User $userIdentity */
+        $userIdentity = \Yii::$app->user->getIdentity();
+        $roleView = "$view-" . $userIdentity->role->value;
+        return
+            file_exists(\Yii::getAlias($this->viewPath . "/" . $roleView . ".php")) ?
+                $roleView : $view;
     }
 
 }

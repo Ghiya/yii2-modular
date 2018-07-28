80241aad682a561baa20ca8f78878c12855b8be5
Ghiya <ghiya.mikadze@gmail.com>
2018-07-28 16:23:12 +0300

    flush records behavior updated

--- src/panel/behaviors/FlushRecordsBehavior.php
+++ src/panel/behaviors/FlushRecordsBehavior.php
@@ -5,65 +5,72 @@
 
 namespace modular\panel\behaviors;
 
+use modular\core\Controller;
 use yii\base\Behavior;
 use yii\base\InvalidConfigException;
 use yii\db\ActiveQuery;
-use yii\db\ActiveRecord;
-use yii\web\Controller;
 
 
 /**
- * Class FlushRecordsBehavior поведение контроллера для автоматического удаления устаревших записей согласно
- * установленным параметрам очистки. При событии [[\yii\web\Controller::EVENT_BEFORE_ACTION]] производит проверку на
- * наличие подходящих записей и удаляет найденные.
+ * @todo    release proper safeDelete functionality
  *
- * @property ActiveRecord $owner
+ * Class FlushRecordsBehavior
+ * Поведение контроллера для автоматического удаления устаревших записей согласно установленным параметрам очистки. При
+ * событии [[\yii\web\Controller::EVENT_BEFORE_ACTION]] производит проверку на наличие подходящих записей и удаляет
+ * найденные.
+ *
+ * @property Controller $owner
  *
  * @package panel\behaviors
- * @author  Ghiya Mikadze <ghiya@mikadze.me>
  */
 class FlushRecordsBehavior extends Behavior
 {
 
 
     /**
-     * @var string $recordClass строковое название класса модели записи
+     * @var string строковое название класса модели записи
      */
     public $recordClass = '';
 
 
     /**
-     * @var int $interval максимальный срок хранения записей ( дни )
+     * @var int максимальный срок хранения записей ( дни )
      */
     public $interval;
 
 
     /**
-     * @var string $attribute аттрибут модели для отбора записей на удаление
+     * @var string аттрибут модели для отбора записей на удаление
      */
     public $attribute = 'created_at';
 
 
     /**
-     * @var string $permission разрешённая RBAC роль пользователя для выполнения очистки
+     * @var string разрешённая RBAC роль пользователя для выполнения очистки
      */
     public $permission;
 
 
     /**
-     * @var int $bottomTimestamp
+     * @var bool если требуется безопасное удаление записей
      */
-    protected $bottomTimestamp;
+    public $isSafeDelete = false;
 
 
     /**
-     * @var int $counter
+     * @var int лимит количества записей обрабатываемых за одну итерацию
      */
-    protected $counter = 0;
+    public $rowsLimit = 10000;
 
 
     /**
-     * @inheritdoc
+     * @var array
+     */
+    private $_messages = [];
+
+
+    /**
+     * {@inheritdoc}
      *
      * @throws InvalidConfigException
      */
@@ -71,16 +78,16 @@ class FlushRecordsBehavior extends Behavior
     {
         parent::init();
         if (empty($this->recordClass)) {
-            throw new InvalidConfigException("Класс модели `recordClass` для автоматической очистки записей должно быть определён.");
+            throw new InvalidConfigException("Property `recordClass` must be defined.");
         }
         if (empty($this->interval)) {
-            throw new InvalidConfigException("Свойство интервала активности `interval` для автоматической очистки должно быть определено.");
+            throw new InvalidConfigException("Property `interval` must be defined.");
         }
     }
 
 
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public function events()
     {
@@ -93,17 +100,136 @@ class FlushRecordsBehavior extends Behavior
 
     /**
      * Производит проверку на наличие записей для очистки и удаляет найденные.
+     *
+     * @throws InvalidConfigException
+     * @throws \Throwable
+     * @throws \yii\db\StaleObjectException
      */
     public function beforeAction()
     {
         if (empty($this->permission) || \Yii::$app->user->can($this->permission)) {
-            $this->flushDeprecated();
-            if (!empty($this->counter)) {
+            $this->flush();
+            $this->resultMessages();
+        }
+    }
+
+
+    /**
+     * @throws \Throwable
+     * @throws \yii\db\StaleObjectException
+     */
+    protected function flush()
+    {
+        $countDeleted = 0;
+        if ($this->isSafeDelete) {
+            foreach ($this->getRows() as $row) {
+                $countDeleted = $row->delete() ? $countDeleted + 1 : $countDeleted;
+            }
+            if (!empty($this->getRows())) {
+                $this->addResultMessage(
+                    "warning",
+                    "Для безопасного удаления всех устарвеших записей требуется повторно перезагружать страницу до исчезновения этого уведомления."
+                );
+            }
+        }
+        else {
+            $countDeleted = $this->getFlushQuery();
+        }
+        if ($countDeleted) {
+            $this->addResultMessage(
+                "Произведена автоматическая очистка <strong>$countDeleted</strong> записей раздела созданных ранее чем <strong>" .
+                \Yii::$app->formatter->asDate($this->getTimestamp()) .
+                "</strong>."
+            );
+        }
+    }
+
+
+    /**
+     * @return array|\yii\db\ActiveRecord[]
+     */
+    protected function getRows()
+    {
+        return
+            $this
+                ->getFilterQuery()
+                ->where($this->getMatchCondition())
+                ->orderBy(
+                    [
+                        $this->attribute => SORT_DESC
+                    ]
+                )
+                ->limit($this->rowsLimit)
+                ->all();
+    }
+
+
+    /**
+     * Возвращает список параметров отбора записей для удаления.
+     *
+     * @return array
+     */
+    protected function getMatchCondition()
+    {
+        return
+            [
+                '<',
+                $this->attribute,
+                $this->getTimestamp()
+            ];
+    }
+
+
+    /**
+     * Выполняет удалений отобранных записей и возвращает их количество.
+     *
+     * @return mixed
+     */
+    protected function getFlushQuery()
+    {
+        return
+            call_user_func(
+                $this->recordClass . '::deleteAll',
+                $this->getMatchCondition()
+            );
+    }
+
+
+    /**
+     * Возвращает объект запроса поиска записей в таблице.
+     *
+     * @return ActiveQuery
+     */
+    protected function getFilterQuery()
+    {
+        return
+            call_user_func(
+                $this->recordClass . '::find'
+            );
+    }
+
+
+    /**
+     * Возвращает значение нижней границы временной метки отбора записей для удаления.
+     *
+     * @return int
+     */
+    protected function getTimestamp()
+    {
+        return strtotime("- $this->interval days");
+    }
+
+
+    /**
+     *
+     */
+    protected function resultMessages()
+    {
+        if (!empty($this->_messages)) {
+            foreach ($this->_messages as $key => $message) {
                 \Yii::$app->session->setFlash(
-                    'info',
-                    "Произведена автоматическая очистка <strong>$this->counter</strong> записей созданных ранее чем <strong>" .
-                    \Yii::$app->formatter->asDatetime($this->getBottomTimestamp(), "php:d.m.Y") .
-                    "</strong>."
+                    $key,
+                    $message
                 );
             }
         }
@@ -111,67 +237,17 @@ class FlushRecordsBehavior extends Behavior
 
 
     /**
-     * Удаляет найденные записи.
-     *
-     * @throws \Exception
-     * @throws \yii\db\StaleObjectException
+     * @param string $message
+     * @param string $key
      */
-    protected function flushDeprecated()
+    protected function addResultMessage($message = "", $key = "info")
     {
-        foreach ($this->getRecords() as $record) {
-            $this->counter = $record->delete() ? $this->counter + 1 : $this->counter;
+        if (isset($this->_messages[$key])) {
+            $this->_messages[$key] .= "\r\n" . $message;
+        }
+        else {
+            $this->_messages[$key] = $message;
         }
     }
 
-
-    /**
-     * Возвращает все записи подходящие под условие отбора.
-     *
-     * @return ActiveRecord[]|null
-     */
-    protected function getRecords()
-    {
-        return $this->getFindQuery()
-            ->where(
-                [
-                    '<',
-                    $this->attribute,
-                    $this->getBottomTimestamp()
-                ]
-            )
-            ->all();
-    }
-
-
-    /**
-     * Возвращает объект запроса поиска записей в базе данных.
-     *
-     * @return ActiveQuery
-     */
-    protected function getFindQuery()
-    {
-        return call_user_func(
-            $this->recordClass . '::find'
-        );
-    }
-
-
-    /**
-     * Возвращает значение нижней границы отбора записей для очистки.
-     *
-     * @return int
-     */
-    protected function getBottomTimestamp()
-    {
-        if (empty($this->bottomTimestamp)) {
-            $this->bottomTimestamp =
-                (int)\Yii::$app->formatter->asTimestamp(
-                    date(
-                        "Y-m-d",
-                        strtotime("- $this->interval days")
-                    ) . " 00:00:00"
-                );
-        }
-        return $this->bottomTimestamp;
-    }
 }

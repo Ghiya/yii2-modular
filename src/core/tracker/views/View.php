fa010559f465a720ee99fd04aa97d19ab2f83790
_ghiya <g.mikadze@lakka.io>
2020-12-22 09:08:02 +0300

    оптимизировано отображение названий в системных уведомлениях (#5)

---
+++ src/core/tracker/views/View.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Copyright (c) 2020 Ghiya Mikadze <ghiya@mikadze.me>
+ */
+
+namespace modular\core\tracker\views;
+
+use modular\core\tracker\controllers\TracksController;
+
+/**
+ * Class View
+ * @package modular\core\tracker\views
+ */
+class View extends \yii\web\View
+{
+
+    /**
+     * @var TracksController
+     */
+    public $context;
+
+}
